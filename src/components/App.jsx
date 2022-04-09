09cf9efae7aa3f4f1e62c730fad33b99a1ae7e60
Diego Rojas <Diego-27ro@outlook.com>
2022-04-09 00:08:57 -0500

    config for project

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Button = styled.button`
+  width: 100%;
+  height: 100%;
+`;
+const App = () => {
+  return (
+    <>
+      <div>App</div>
+      <Button> hola</Button>
+    </>
+  );
+};
+
+export default App;
---
+++ webpack.config.dev.js
@@ -0,0 +1,73 @@
+/** @type {import('webpack').Configuration} */
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.[contenthash].js',
+    assetModuleFilename: 'assets/[hash][ext]',
+    clean: true,
+    publicPath: '/',
+  },
+  mode: 'development',
+  resolve: {
+    extensions: ['.js', '.jsx', '.json', '.scss'],
+    alias: {},
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            cacheDirectory: true,
+            cacheCompression: false,
+            envName: 'development',
+          },
+        },
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
+      },
+      {
+        test: /\.(jpg|png|jpeg|svg)$/,
+        use: {
+          loader: 'url-loader',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: './public/index.html',
+      filename: './index.html',
+    }),
+
+    new MiniCssExtractPlugin({
+      filename: 'assets/[name].[contenthash].css',
+    }),
+
+    //new BundleAnalyzerPlugin(),
+  ],
+  devServer: {
+    historyApiFallback: true,
+    static: './dist',
+    compress: true,
+    port: 3000,
+  },
+};
