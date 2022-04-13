a7892729d73bf5a5f19ca3b8ddc25c59fc2b0442
Diego Rojas <Diego-27ro@outlook.com>
2022-04-13 01:07:53 -0500

    add helmet to home

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import { Products } from '@components/Products';
-
+import { Helmet } from 'react-helmet';
 const Home = () => {
-    return <Products></Products>;
+    return (
+        <>
+            <Helmet>
+                <title>Platzi Conf Mer - Products </title>
+            </Helmet>
+            <Products></Products>;
+        </>
+    );
 };
 
 export { Home };
