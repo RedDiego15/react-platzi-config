83fea02b7b0c94974838227c82e7b21b68f1af8b
Diego Rojas <Diego-27ro@outlook.com>
2022-04-12 22:46:35 -0500

    add firebase hosting

--- webpack.config.js
+++ webpack.config.js
@@ -5,70 +5,75 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const BundleAnalyzerPlugin =
+    require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.[contenthash].js',
-    clean: true,
-  },
-  mode: 'production',
-  resolve: {
-    extensions: ['.mjs','.js', '.jsx','.json'],
-    alias: {
-      '@containers': path.join(__dirname, './src/containers'),
-      '@components': path.join(__dirname, './src/components'),
-      '@routes': path.join(__dirname, './src/routes'),
-      '@styles': path.join(__dirname, './src/styles'),
-    }
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: 'html-loader',
-          },
-        ],
-      },
-      {
-        test: /\.(scss|csss)$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
-      },
-      {
-        test: /\.(jpg|png|jpeg|svg)$/,
-        use: {
-          loader: 'url-loader',
-        },
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      inject: true,
-      template: './public/index.html',
-      filename: './index.html',
-    }),
-    new MiniCssExtractPlugin({
-      filename: 'assets/[name].[contenthash].css',
-    }),
-    new BundleAnalyzerPlugin(),
-  ],
-  optimization: {
-    minimize: true,
-    minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
-    splitChunks: {
-      chunks: 'all',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.[contenthash].js',
+        clean: true,
     },
-  },
+    mode: 'production',
+    resolve: {
+        extensions: ['.mjs', '.js', '.jsx', '.json'],
+        alias: {
+            '@containers': path.join(__dirname, './src/containers'),
+            '@components': path.join(__dirname, './src/components'),
+            '@routes': path.join(__dirname, './src/routes'),
+            '@styles': path.join(__dirname, './src/styles'),
+            '@hooks': path.join(__dirname, './src/hooks'),
+            '@context': path.join(__dirname, './src/context'),
+        },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader',
+                    },
+                ],
+            },
+            {
+                test: /\.(scss|csss)$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.(jpg|png|jpeg|svg)$/,
+                use: {
+                    loader: 'url-loader',
+                },
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: './public/index.html',
+            filename: './index.html',
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'assets/[name].[contenthash].css',
+        }),
+        new BundleAnalyzerPlugin(),
+        new Dotenv(),
+    ],
+    optimization: {
+        minimize: true,
+        minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
+        splitChunks: {
+            chunks: 'all',
+        },
+    },
 };
